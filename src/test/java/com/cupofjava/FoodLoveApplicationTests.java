4413d929121b90ce16c495a3846eaca340a086a2
oskardz <oskardz1991@gmail.com>
2017-07-28 11:08:22 +0200

    Initial Commit

---
+++ src/test/java/com/cupofjava/FoodLoveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cupofjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodLoveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
