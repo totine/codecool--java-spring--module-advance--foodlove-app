86ea511600f5f33e5ce2503f398c0ca855670618
oskardz <oskardz1991@gmail.com>
2017-08-29 13:10:50 +0200

    Add sample template and route for restaurator login.

---
+++ src/main/java/com/cupofjava/controllers/RestauratorController.java
@@ -0,0 +1,17 @@
+package com.cupofjava.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by oskar on 29.08.17.
+ */
+@Controller
+public class RestauratorController{
+
+    @RequestMapping("/restaurators/login")
+    public String loginPage(){
+        return "restaurator/login";
+    }
+
+}
