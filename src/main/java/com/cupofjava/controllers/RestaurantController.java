56851bbf35949dda43a412047814cf577e02bef7
Jakubwlodarczyk <Kubawl.91@gmail.com>
2017-09-06 10:46:40 +0200

    Delete unused import from RestaurantController

--- src/main/java/com/cupofjava/controllers/RestaurantController.java
+++ src/main/java/com/cupofjava/controllers/RestaurantController.java
@@ -1,7 +1,6 @@
 package com.cupofjava.controllers;
 
 import com.cupofjava.services.RestaurantService;
-import com.cupofjava.services.RestauratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
