3dbcc16d4ac7572670dbc80ddadc23648b0eee5d
Jakubwlodarczyk <Kubawl.91@gmail.com>
2017-09-20 22:00:51 +0200

    Add if statement to RestauratorController to prevent multiple form creation

--- src/main/java/com/cupofjava/controllers/RestauratorController.java
+++ src/main/java/com/cupofjava/controllers/RestauratorController.java
@@ -1,8 +1,6 @@
 package com.cupofjava.controllers;
 
-import com.cupofjava.domain.Attribute;
-import com.cupofjava.domain.Restaurant;
-import com.cupofjava.domain.Restaurator;
+import com.cupofjava.domain.*;
 import com.cupofjava.services.AttributeService;
 import com.cupofjava.services.RestaurantService;
 import com.cupofjava.services.RestauratorService;
@@ -41,82 +39,86 @@ public class RestauratorController{
 
     @RequestMapping("/restaurators/create")
     public String createBasicRestaurantorsRestaurantsAttributes(){
-        Restaurant restaurant1 = new Restaurant("Marcello Ristorante", "Połnocna 2 Krakow");
-        Restaurant restaurant2 = new Restaurant("U Funkiera", "Zachodnia 2 Kraków");
-        Restaurant restaurant3 = new Restaurant("Atelier Amaro", "Ślusarska 2 Krakow");
-        Restaurant restaurant4 = new Restaurant("Studio Kulinarne", "Inna 2 Kraków");
 
-        Set<Restaurant> restaurantSet = new HashSet<>();
-        restaurantSet.add(restaurant1);
-        restaurantSet.add(restaurant2);
+        if (restauratorService.listAll().size() == 0) {
+            Restaurant restaurant1 = new Restaurant("Marcello Ristorante", "Połnocna 2 Krakow");
+            Restaurant restaurant2 = new Restaurant("U Funkiera", "Zachodnia 2 Kraków");
+            Restaurant restaurant3 = new Restaurant("Atelier Amaro", "Ślusarska 2 Krakow");
+            Restaurant restaurant4 = new Restaurant("Studio Kulinarne", "Inna 2 Kraków");
 
-        Set<Restaurant> restaurantSet2 = new HashSet<>();
-        restaurantSet2.add(restaurant3);
-        restaurantSet2.add(restaurant4);
+            Set<Restaurant> restaurantSet = new HashSet<>();
+            restaurantSet.add(restaurant1);
+            restaurantSet.add(restaurant2);
 
-        Restaurator restaurator = new Restaurator("Magda Gessler", restaurantSet);
-        Restaurator restaurator2 = new Restaurator("Modest Amaro", restaurantSet2);
+            Set<Restaurant> restaurantSet2 = new HashSet<>();
+            restaurantSet2.add(restaurant3);
+            restaurantSet2.add(restaurant4);
 
-        restaurantService.saveOrUpdate(restaurant1);
-        restaurantService.saveOrUpdate(restaurant2);
-        restaurantService.saveOrUpdate(restaurant3);
-        restaurantService.saveOrUpdate(restaurant4);
+            Restaurator restaurator = new Restaurator("Magda Gessler", restaurantSet);
+            Restaurator restaurator2 = new Restaurator("Modest Amaro", restaurantSet2);
 
-        restauratorService.saveOrUpdate(restaurator);
-        restauratorService.saveOrUpdate(restaurator2);
+            restaurantService.saveOrUpdate(restaurant1);
+            restaurantService.saveOrUpdate(restaurant2);
+            restaurantService.saveOrUpdate(restaurant3);
+            restaurantService.saveOrUpdate(restaurant4);
 
-        restaurant1.setRestaurator(restaurator);
-        restaurant2.setRestaurator(restaurator);
+            restauratorService.saveOrUpdate(restaurator);
+            restauratorService.saveOrUpdate(restaurator2);
 
-        restaurant3.setRestaurator(restaurator2);
-        restaurant4.setRestaurator(restaurator2);
+            restaurant1.setRestaurator(restaurator);
+            restaurant2.setRestaurator(restaurator);
 
-        restaurantService.saveOrUpdate(restaurant1);
-        restaurantService.saveOrUpdate(restaurant2);
-        restaurantService.saveOrUpdate(restaurant3);
-        restaurantService.saveOrUpdate(restaurant4);    ///Trzeba zrobić to jakoś w konstruktorze(chyba), zeby samo dodawało własciciela do restauracji, ale to skomplikowane.
+            restaurant3.setRestaurator(restaurator2);
+            restaurant4.setRestaurator(restaurator2);
 
-        Attribute glutenFree = new Attribute("Gluten free");
-        Attribute lactoseFree = new Attribute("Lactose free");
-        Attribute sugarFree = new Attribute("Sugar free");
-        Attribute vegan = new Attribute("Vegan");
-        Attribute vegetarian = new Attribute("Vegetarian");
-        Attribute soup = new Attribute("Soup");
-        Attribute meat = new Attribute("Meat");
-        Attribute sweets = new Attribute("Sweets");
-        Attribute dessert = new Attribute("Dessert");
-        Attribute salad = new Attribute("Salad");
-        Attribute sandwich = new Attribute("Sandwich");
-        Attribute beef = new Attribute("Beef");
-        Attribute chicken = new Attribute("Chicken");
-        Attribute pork = new Attribute("Pork");
-        Attribute curry = new Attribute("Curry");
-        Attribute quiche = new Attribute("Quiche");
-        Attribute beverage = new Attribute("Beverage");
-        Attribute coffee = new Attribute("Coffee");
-        Attribute juice = new Attribute("Juice");
-        Attribute smoothie = new Attribute("Smoothie");
+            restaurantService.saveOrUpdate(restaurant1);
+            restaurantService.saveOrUpdate(restaurant2);
+            restaurantService.saveOrUpdate(restaurant3);
+            restaurantService.saveOrUpdate(restaurant4);    ///Trzeba zrobić to jakoś w konstruktorze(chyba), zeby samo dodawało własciciela do restauracji, ale to skomplikowane.
 
-        attributeService.saveOrUpdate(glutenFree);
-        attributeService.saveOrUpdate(lactoseFree);
-        attributeService.saveOrUpdate(sugarFree);
-        attributeService.saveOrUpdate(vegan);
-        attributeService.saveOrUpdate(vegetarian);
-        attributeService.saveOrUpdate(soup);
-        attributeService.saveOrUpdate(meat);
-        attributeService.saveOrUpdate(sweets);
-        attributeService.saveOrUpdate(dessert);
-        attributeService.saveOrUpdate(salad);
-        attributeService.saveOrUpdate(sandwich);
-        attributeService.saveOrUpdate(beef);
-        attributeService.saveOrUpdate(chicken);
-        attributeService.saveOrUpdate(pork);
-        attributeService.saveOrUpdate(curry);
-        attributeService.saveOrUpdate(quiche);
-        attributeService.saveOrUpdate(beverage);
-        attributeService.saveOrUpdate(coffee);
-        attributeService.saveOrUpdate(juice);
-        attributeService.saveOrUpdate(smoothie);
+            Attribute glutenFree = new Attribute("Gluten free");
+            Attribute lactoseFree = new Attribute("Lactose free");
+            Attribute sugarFree = new Attribute("Sugar free");
+            Attribute vegan = new Attribute("Vegan");
+            Attribute vegetarian = new Attribute("Vegetarian");
+            Attribute soup = new Attribute("Soup");
+            Attribute meat = new Attribute("Meat");
+            Attribute sweets = new Attribute("Sweets");
+            Attribute dessert = new Attribute("Dessert");
+            Attribute salad = new Attribute("Salad");
+            Attribute sandwich = new Attribute("Sandwich");
+            Attribute beef = new Attribute("Beef");
+            Attribute chicken = new Attribute("Chicken");
+            Attribute pork = new Attribute("Pork");
+            Attribute curry = new Attribute("Curry");
+            Attribute quiche = new Attribute("Quiche");
+            Attribute beverage = new Attribute("Beverage");
+            Attribute coffee = new Attribute("Coffee");
+            Attribute juice = new Attribute("Juice");
+            Attribute smoothie = new Attribute("Smoothie");
+
+            attributeService.saveOrUpdate(glutenFree);
+            attributeService.saveOrUpdate(lactoseFree);
+            attributeService.saveOrUpdate(sugarFree);
+            attributeService.saveOrUpdate(vegan);
+            attributeService.saveOrUpdate(vegetarian);
+            attributeService.saveOrUpdate(soup);
+            attributeService.saveOrUpdate(meat);
+            attributeService.saveOrUpdate(sweets);
+            attributeService.saveOrUpdate(dessert);
+            attributeService.saveOrUpdate(salad);
+            attributeService.saveOrUpdate(sandwich);
+            attributeService.saveOrUpdate(beef);
+            attributeService.saveOrUpdate(chicken);
+            attributeService.saveOrUpdate(pork);
+            attributeService.saveOrUpdate(curry);
+            attributeService.saveOrUpdate(quiche);
+            attributeService.saveOrUpdate(beverage);
+            attributeService.saveOrUpdate(coffee);
+            attributeService.saveOrUpdate(juice);
+            attributeService.saveOrUpdate(smoothie);
+        }
+
 
         return "redirect:/restaurators/login";
     }
